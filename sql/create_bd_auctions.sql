84f6a8adddc407e2e8708c7fa081bee1325f7c24
fleborgne2025 <francisleborgne@gmx.com>
2025-04-28 13:01:52 +0200

    Ajout du sql création des tables

---
+++ sql/create_bd_auctions.sql
@@ -0,0 +1,99 @@
+-- Script de création de la base de données ENCHERES
+--   type :      SQL Server 2012
+--
+USE Auctions;
+GO
+
+DROP TABLE IF EXISTS AUCTIONS;
+DROP TABLE IF EXISTS PICKUPS;
+DROP TABLE IF EXISTS USERS;
+DROP TABLE IF EXISTS ITEMS_SOLD;
+DROP TABLE IF EXISTS CATEGORIES;
+
+CREATE TABLE CATEGORIES (
+    category_id   INTEGER IDENTITY(1,1) NOT NULL,
+    category_desc        VARCHAR(30) NOT NULL
+)
+
+ALTER TABLE CATEGORIES ADD constraint categorie_pk PRIMARY KEY (category_id)
+
+CREATE TABLE AUCTIONS (
+    user_id   INTEGER NOT NULL,
+    item_id       INTEGER NOT NULL,
+    bid_date     datetime NOT NULL,
+	bid_price  INTEGER NOT NULL
+
+)
+
+ALTER TABLE AUCTIONS ADD constraint enchere_pk PRIMARY KEY (user_id, item_id)
+
+CREATE TABLE PICKUPS (
+	item_id         INTEGER NOT NULL,
+    address              VARCHAR(30) NOT NULL,
+    post_code      VARCHAR(15) NOT NULL,
+    city            VARCHAR(30) NOT NULL
+)
+
+ALTER TABLE PICKUPS ADD constraint pickup_pk PRIMARY KEY  (item_id)
+
+CREATE TABLE USERS (
+    user_id   INTEGER IDENTITY(1,1) NOT NULL,
+    pseudo           VARCHAR(30) NOT NULL,
+    lastname             VARCHAR(30) NOT NULL,
+    firstname           VARCHAR(30) NOT NULL,
+    email            VARCHAR(20) NOT NULL,
+    phone_nb        VARCHAR(15),
+    address              VARCHAR(30) NOT NULL,
+    post_code      VARCHAR(10) NOT NULL,
+    city            VARCHAR(30) NOT NULL,
+    password     VARCHAR(30) NOT NULL,
+    credit           INTEGER NOT NULL,
+    administrator   bit NOT NULL
+)
+
+ALTER TABLE USERS ADD constraint user_pk PRIMARY KEY (user_id)
+
+
+CREATE TABLE ITEMS_SOLD (
+    item_id                    INTEGER IDENTITY(1,1) NOT NULL,
+    item_name                   VARCHAR(30) NOT NULL,
+    description                   VARCHAR(300) NOT NULL,
+	auction_date_begin           DATE NOT NULL,
+    auction_date_end             DATE NOT NULL,
+    price_init                  INTEGER,
+    price_selling                    INTEGER,
+    user_id               INTEGER NOT NULL,
+    category_id                INTEGER NOT NULL
+)
+
+ALTER TABLE ITEMS_SOLD ADD constraint items_sold_pk PRIMARY KEY (item_id)
+
+ALTER TABLE ITEMS_SOLD
+    ADD CONSTRAINT user_auction_fk FOREIGN KEY ( user_id ) REFERENCES USERS ( user_id )
+ON DELETE NO ACTION 
+    ON UPDATE no action 
+
+ALTER TABLE AUCTIONS
+    ADD CONSTRAINT	auctions_items_fk FOREIGN KEY ( item_id )
+        REFERENCES ITEMS_SOLD ( item_id )
+ON DELETE NO ACTION 
+    ON UPDATE no action 
+
+ALTER TABLE PICKUPS
+    ADD CONSTRAINT pickup_items_sold_fk FOREIGN KEY ( item_id )
+        REFERENCES ITEMS_SOLD ( item_id )
+ON DELETE NO ACTION 
+    ON UPDATE no action 
+
+ALTER TABLE ITEMS_SOLD
+    ADD CONSTRAINT categories_items_sold_fk FOREIGN KEY ( category_id )
+        REFERENCES categories ( category_id )
+ON DELETE NO ACTION 
+    ON UPDATE no action 
+
+ALTER TABLE ITEMS_SOLD
+    ADD CONSTRAINT user_sold_fk FOREIGN KEY ( user_id )
+        REFERENCES users ( user_id )
+ON DELETE NO ACTION 
+    ON UPDATE no action 
+
