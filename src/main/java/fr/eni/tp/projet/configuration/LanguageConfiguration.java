c4260d5b1cda89119eb970bb8fb68b37aeb6dd7c
kcorre2025 <kevin.corre2025@campus-eni.fr>
2025-04-30 13:54:47 +0200

    Formulaire de création de profile + modifications html / css

--- src/main/java/fr/eni/tp/projet/configuration/LanguageConfiguration.java
+++ src/main/java/fr/eni/tp/projet/configuration/LanguageConfiguration.java
@@ -1,4 +1,34 @@
 package fr.eni.tp.projet.configuration;
 
-public class LanguageConfiguration {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@Configuration
+public class LanguageConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    LocaleResolver localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(new Locale("fr"));
+        return sessionLocaleResolver;
+    }
+
+    @Bean
+    LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry interceptorRegistry) {
+        interceptorRegistry.addInterceptor(localeChangeInterceptor());
+    }
 }
